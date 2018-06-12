59ba50b10e7bf0b18d2de171d041004ce780f97e
Shriman Nijagun <EVOLVUS\shrimank@ELPBLR-025.evolvus.com>
2018-06-12 15:06:49 +0530

    server.js added.

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ var axios = require('axios');
 
 const appId = process.env.MicrosoftAppId || 'c718e84f-a303-4787-850d-e53f1928a804';
 const appPassword = process.env.MicrosoftAppPassword || 'tdfHHOXL5{(%*zsnqUQ7011';
-const dianaNlpUrl= process.env.DIANA_NLP_SERVICE_URL || 'https://lit-inlet-30311.herokuapp.com/api/v0.1/diana';
+const dianaNlpUrl = process.env.DIANA_NLP_SERVICE_URL || 'https://lit-inlet-30311.herokuapp.com/api/v0.1/diana';
 
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
+    console.log('%s listening to %s', server.name, server.url);
 });
 
 // Create chat connector for communicating with the Bot Framework Service
@@ -23,15 +23,18 @@ server.post('/api/messages', connector.listen());
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session) {
-    
+
+    console.log(`${dianaNlpUrl}?query=${session.message.text}`);
 
     axios.post(`${dianaNlpUrl}?query=${session.message.text}`)
-            .then(function (response) {
-                    console.log(response.body);
-            })
-            .catch(function (error) {
-                    console.log(error);
-            });
+        .then(function (response) {
+            console.log(response.body);
+            session.send(response.body.displayText[0]);
+        })
+        .catch(function (error) {
+            console.log(error);
+            session.send(error);
+        });
 
 
 
@@ -40,6 +43,6 @@ var bot = new builder.UniversalBot(connector, function (session) {
 });
 
 
-function prepareResponse(standardResponse){
+function prepareResponse(standardResponse) {
 
 }
