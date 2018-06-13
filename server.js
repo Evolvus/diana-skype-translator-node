037ac327094197efc4742682514b44b86ec42099
Shriman Nijagun <EVOLVUS\shrimank@ELPBLR-025.evolvus.com>
2018-06-13 09:53:49 +0530

    server.js added.

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ var bot = new builder.UniversalBot(connector, function (session) {
             const body = response.data.body;
             const msgResponse = prepareResponse(body);
             console.log("Response Sending ",msgResponse);
+            console.log("Response Sending JSON content",JSON.parse(msgResponse.attachments[0].content,null,2));
             session.send(msgResponse);
         }).catch(function (error) {
             console.log("ERROR", error);
